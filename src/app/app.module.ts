e5165cea12b30ea7e635d7b013701cf3ee522b76
dino3616 <suraimu3616@outlook.jp>
2022-06-12 01:04:36 +0900

    feat: ✨ 簡易なGraphQLサーバーを実装.

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import PostsModule from '../components/post/post.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), './schema.gql'),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
+})
+export default class AppModule {}
