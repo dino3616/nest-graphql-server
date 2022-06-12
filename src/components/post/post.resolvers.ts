4e5ceb7bed22e863f745f5547cb095f2a4bbfb6e
dino3616 <suraimu3616@outlook.jp>
2022-06-12 20:20:55 +0900

    feat: ✨ envTestクエリを実装.

--- src/components/post/post.resolvers.ts
+++ src/components/post/post.resolvers.ts
@@ -1,8 +1,11 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import Env from '../../config/environment/getter/getter.service';
 import PostModel from './post.model';
 
 @Resolver(() => PostModel)
 export default class PostResolver {
+  constructor(private env: Env) {}
+
   // eslint-disable-next-line class-methods-use-this
   @Query(() => [PostModel], { name: 'posts', nullable: true })
   async getPosts() {
@@ -10,5 +13,10 @@ export default class PostResolver {
       { id: '1', title: 'yukarisan-kawaii-yatta-' },
       { id: '2', title: 'mikusanmo-kawaii-yatta-' },
     ];
+  }
+
+  @Query(() => String)
+  envTest(): string {
+    return this.env.DatabaseURL;
   }
 }
