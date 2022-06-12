726352cea8773ab34076ede8e95a5282f76f57fe
dino3616 <suraimu3616@outlook.jp>
2022-06-12 14:20:32 +0900

    fix: 🐛 getPosts()を静的メソッドから通常のメソッドに変更.

--- src/components/post/post.resolvers.ts
+++ src/components/post/post.resolvers.ts
@@ -3,8 +3,9 @@ import PostModel from './post.model';
 
 @Resolver(() => PostModel)
 export default class PostResolver {
+  // eslint-disable-next-line class-methods-use-this
   @Query(() => [PostModel], { name: 'posts', nullable: true })
-  static async getPosts() {
+  async getPosts() {
     return [
       { id: '1', title: 'yukarisan-kawaii-yatta-' },
       { id: '2', title: 'mikusanmo-kawaii-yatta-' },
