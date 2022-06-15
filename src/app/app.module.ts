cf04adc7144422f9f73b0c33c7ea28dc20efae66
dino3616 <suraimu3616@outlook.jp>
2022-06-16 00:26:13 +0900

    fix: 🩹 GraphQLに関する設定を別ファイルに分離.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,10 @@
-import { join } from 'path';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import Components from '../components';
 import EnvModule from '../config/environment/getter/getter.module';
+import GraphQLConfig from '../config/graphql/graphql.config';
 import PrismaModule from '../libs/prisma/prisma.module';
 
 @Module({
-  imports: [
-    EnvModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      path: '/graphql',
-      introspection: true,
-      autoSchemaFile: join(process.cwd(), './schema.gql'),
-      sortSchema: true,
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-    }),
-    PrismaModule,
-    ...Components,
-  ],
+  imports: [EnvModule, GraphQLConfig, PrismaModule, ...Components],
 })
 export default class AppModule {}
